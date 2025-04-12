d3e1e47c3497a1690fa72b956498c0b1a0439444
RobinSardja <robinsardja@gmail.com>
2025-04-12 16:48:50 -0400

    hardcoded records

--- lib/records.dart
+++ lib/records.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 import 'package:google_generative_ai/google_generative_ai.dart';
@@ -27,6 +29,114 @@ class _RecordsState extends State<Records> {
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return ListView(
+      children: [
+        ListTile(
+          leading: Icon( Icons.folder ),
+          onTap: () { Navigator.push( context, MaterialPageRoute( builder: (context) => Decipher() ) ); },
+          title: Text( "New Record: Meeting Setup" ),
+          subtitle: Text( "Difficulty: New Recruit" )
+        ),
+        ListTile(
+          leading: Icon( Icons.folder ),
+          onTap: () { Navigator.push( context, MaterialPageRoute( builder: (context) => Decipher() ) ); },
+          title: Text( "New Record: Package Handover" ),
+          subtitle: Text( "Difficulty: Field Operative" )
+        ),
+        ListTile(
+          leading: Icon( Icons.folder ),
+          onTap: () { Navigator.push( context, MaterialPageRoute( builder: (context) => Decipher() ) ); },
+          title: Text( "New Record: Sales Pitch" ),
+          subtitle: Text( "Difficulty: Special Agent" )
+        ),
+        ListTile(
+          leading: Icon( Icons.folder ),
+          onTap: () { Navigator.push( context, MaterialPageRoute( builder: (context) => Decipher() ) ); },
+          title: Text( "New Record: Business Deal" ),
+          subtitle: Text( "Difficulty: Super Spy" )
+        )
+      ]
+    );
+  }
+}
+
+class Decipher extends StatefulWidget {
+  const Decipher({super.key});
+
+  @override
+  State<Decipher> createState() => _DecipherState();
+}
+
+class _DecipherState extends State<Decipher> {
+  Duration elapsedTime = Duration.zero;
+  bool showTimer = false;
+  late Timer timer;
+
+  String formatTime() {
+    final minutes = elapsedTime.inMinutes.remainder(60).toString().padLeft(2, '0');
+    final seconds = elapsedTime.inSeconds.remainder(60).toString().padLeft(2, '0');
+    final milliseconds = ( elapsedTime.inMilliseconds.remainder(1000) / 10 ).toStringAsFixed(0).padLeft(2, '0');
+
+    return "$minutes:$seconds:$milliseconds";
+  }
+
+  void startStopwatch() {
+    timer = Timer.periodic( Duration( milliseconds: 10 ), (timer) {
+      setState( () => elapsedTime += Duration( milliseconds: 10 ) );
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    startStopwatch();
+  }
+
+  @override
+  void dispose() {
+    if( timer.isActive ) timer.cancel();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon( Icons.timer ),
+          onPressed: () => showTimer = !showTimer
+        ),
+        actions: [
+          IconButton(
+            icon: Icon( Icons.close ),
+            onPressed: () => showDialog(
+              builder: (context) => AlertDialog(
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                      Navigator.pop(context);
+                    },
+                    child: Text( "Yes" )
+                  ),
+                  TextButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: Text( "No" )
+                  )
+                ],
+                content: Text( "Are you sure you want to quit?" ),
+                title: Text( "Quitting" )
+              ),
+              context: context
+            )
+          )
+        ],
+        automaticallyImplyLeading: false,
+        title: Text( showTimer ? formatTime() : "Deciphering Record" )
+      ),
+      body: Center( child: Text( "Decipher" ) )
+    );
   }
 }
