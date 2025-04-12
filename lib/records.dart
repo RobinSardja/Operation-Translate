d2fe1e74df123dc47b0049ecde4a83868cc3f386
RobinSardja <robinsardja@gmail.com>
2025-04-12 15:12:03 -0400

    gemini piping

--- lib/records.dart
+++ lib/records.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:google_generative_ai/google_generative_ai.dart';
+
 class Records extends StatefulWidget {
   const Records({super.key});
 
@@ -8,8 +10,37 @@ class Records extends StatefulWidget {
 }
 
 class _RecordsState extends State<Records> {
+  static const apiKey = String.fromEnvironment( "gemini", defaultValue: "none" );
+  String output = "Click the button above to generate!";
+
+  Future<String> pipe( String prompt ) async {
+    final model = GenerativeModel(
+        model: 'gemini-1.5-flash-latest',
+        apiKey: apiKey
+    );
+
+    final content = [ Content.text(prompt) ];
+    final response = await model.generateContent(content);
+
+    return response.text ?? "ERROR";
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center( child: Text( "Records" ) );
+    return ListView(
+      children: [
+        ListTile(
+          title: FloatingActionButton(
+            onPressed: () async {
+              output = await pipe( "Hello! How are you?" );
+              setState(() {});
+            }
+          )
+        ),
+        ListTile(
+          title: Text( output )
+        )
+      ]
+    );
   }
 }
